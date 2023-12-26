dd3def408a294c7f8f65e4a61036cf012ce54904
Samrat Kamthe <samratkamthe11@gmail.com>
2023-12-27 00:18:31 +0530

    login page edit

--- src/components/Login.js
+++ src/components/Login.js
@@ -24,7 +24,7 @@ const Login = (props) => {
     //   return json.name; 
     // };
           
-          console.log(json)
+          // console.log(json)
           if(json.success){
             //save the auth token and redirect to the next page
             localStorage.setItem('token',json.authtoken);
